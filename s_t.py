5122bb532870100eb82d6b79c7d5a5de5caad289
Japan <106416229+ElSaanti@users.noreply.github.com>
2026-03-05 07:01:42 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -29,7 +29,7 @@ with st.sidebar:
                  " la configuración de lenguaje que necesites.")
 
 
-st.write("Toca el Botón y habla lo que te gustaria traducir")
+st.write("Toca el botón y habla lo que te gustaria traducir excepto si tienes iPhone 🫠")
 
 stt_button = Button(label=" Escuchar  🎤", width=300,  height=50)
 
